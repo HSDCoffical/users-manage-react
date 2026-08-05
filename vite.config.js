f1a0fcf4d0e48229f62579fdb76bc343dcbf6220
凉数中 <2791389901@qq.com>
2026-08-05 11:50:50 +0800

    更新 vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  root: './',  // 显式指定根目录
+  root: __dirname,  // 强制使用项目根目录的绝对路径
   plugins: [tailwindcss(), react()],
+  server: {
+    host: '0.0.0.0',
+    port: 8976,
+    allowedHosts: ['*']
+  }
 });
